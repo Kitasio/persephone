7a2a2e6162089032d88a4bec182b5d8bec3c5c14
Kitasio <kit.plotits@gmail.com>
2025-05-15 17:42:27 +0200

    Add footer image

--- src/components/GentleInvitation.tsx
+++ src/components/GentleInvitation.tsx
@@ -4,7 +4,7 @@ const GentleInvitation: React.FC = () => {
   return (
     <div className="mt-24 md:mt-48 mb-24 md:mb-48">
       <h2 className="text-5xl xl:text-[70px] font-serif italic mb-12 xl:mb-24 text-neutral-800">A Gentle Invitation</h2>
-      
+
       <div className="max-w-[86rem] mx-auto">
         <div className="space-y-8">
           <p className="whitespace-pre-line text-neutral-900 font-roboto text-xl xl:text-3xl/[1.5] font-light leading-relaxed text-pretty">
@@ -12,7 +12,7 @@ const GentleInvitation: React.FC = () => {
             
             Explore what calls to you, and let this space support you however you need. Persephone's Playground is here whenever you need to feel held, understood, and reminded that your journey has meaning.`}
           </p>
-          
+
           <p className="text-neutral-900 font-roboto text-xl xl:text-3xl/[1.5] font-light leading-relaxed text-pretty">
             May you find small moments of comfort here, and may you carry them with you as you walk through this time.
           </p>
@@ -29,7 +29,11 @@ const GentleInvitation: React.FC = () => {
           </div>
         </div>
 
-        <div className="mt-24 md:mt-32">
+        <div className="relative mt-20">
+          <img className="absolute scale-125" alt="flowers" src="https://ik.imagekit.io/soulgenesis/Persephone/flowers?updatedAt=1747322935888" />
+        </div>
+
+        <div className="mt-[50vh] sm:mt-[60vh] lg:mt-[80vh] xl:mt-[100vh] 2xl:mt-[120vh]">
           <p className="whitespace-pre-line text-xl xl:text-2xl italic text-neutral-700 mb-6 text-center">
             {`This page is dedicated to all the Persephones I've been blessed to know, 
             as well as the men and women who have supported them 
